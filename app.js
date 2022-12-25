0dbc1f33d7d9c2b70c2b2a5d4a925ce366d26e2a
Taha Sökmen <resimli1000@gmail.com>
2022-12-26 01:09:22 +0300

    implemented mssql

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const sql = require("mssql");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -38,52 +37,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
-const config = {
-  user: "admin-team2", // better stored in an app setting such as process.env.DB_USER
-  password: "&^BcGq71*3p1", // better stored in an app setting such as process.env.DB_PASSWORD
-  server: "team2sqlserver.database.windows.net", // better stored in an app setting such as process.env.DB_SERVER
-  port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
-  database: "employeeDB", // better stored in an app setting such as process.env.DB_NAME
-  authentication: {
-    type: "default",
-  },
-  options: {
-    encrypt: true,
-  },
-};
-
-app.get("/list", (req, res, next) => {
-  getAllRows().then((x) => res.json(x));
-});
-
-app.get("/delete/:employeePhone", (req, res, next) => {
-  deleteByPhone(req.params.employeePhone);
-});
-
-async function getAllRows() {
-  try {
-    var poolConnection = await sql.connect(config);
-    var resultSet = await poolConnection
-      .request()
-      .query(`SELECT TOP (1000) * FROM [dbo].[tblEmployee]`);
-    poolConnection.close();
-    return resultSet.recordset;
-  } catch (err) {
-    console.error(err.message);
-  }
-}
-
-async function deleteByPhone(phone) {
-  try {
-    var poolConnection = await sql.connect(config);
-    var resultSet = await poolConnection
-      .request()
-      .query(`DELETE FROM tblEmployee WHERE EmpPhone = ${phone};`);
-    poolConnection.close();
-  } catch (err) {
-    console.error(err.message);
-  }
-}
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,57 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const sql = require("mssql");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+const config = {
+  user: "admin-team2", // better stored in an app setting such as process.env.DB_USER
+  password: "&^BcGq71*3p1", // better stored in an app setting such as process.env.DB_PASSWORD
+  server: "team2sqlserver.database.windows.net", // better stored in an app setting such as process.env.DB_SERVER
+  port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+  database: "employeeDB", // better stored in an app setting such as process.env.DB_NAME
+  authentication: {
+    type: "default",
+  },
+  options: {
+    encrypt: true,
+  },
+};
+
+router.get("/list", (req, res, next) => {
+  getAllRows().then((x) => res.json(x));
+});
+
+router.get("/delete/:employeePhone", (req, res, next) => {
+  deleteByPhone(req.params.employeePhone);
+});
+
+async function getAllRows() {
+  try {
+    var poolConnection = await sql.connect(config);
+    var resultSet = await poolConnection
+      .request()
+      .query(`SELECT TOP (1000) * FROM [dbo].[tblEmployee]`);
+    poolConnection.close();
+    return resultSet.recordset;
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function deleteByPhone(phone) {
+  try {
+    var poolConnection = await sql.connect(config);
+    var resultSet = await poolConnection
+      .request()
+      .query(`DELETE FROM tblEmployee WHERE EmpPhone = ${phone};`);
+    poolConnection.close();
+  } catch (err) {
+    console.error(err.message);
+  }
+}
 
 module.exports = router;
