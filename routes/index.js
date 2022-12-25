4f32dc8e9cc5d74f6d7e3f10f643a10de700412d
Taha Sökmen <resimli1000@gmail.com>
2022-12-26 01:15:54 +0300

    disabled mssql

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const sql = require("mssql");
+//const sql = require("mssql");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
